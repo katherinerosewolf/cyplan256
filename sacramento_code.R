5006f6f02ee1fc2d93e34e13351eb213940a177a
Katherine Rose Wolf <katherinerosewolf@gmail.com>
2018-12-12 03:32:20 -0800

    hewwo

--- sacramento_code.R
+++ sacramento_code.R
@@ -19,6 +19,7 @@ library(tcltk2)
 library(bit64)
 library(scales)
 library(forcats)
+library(reshape2)
 
 
 
@@ -79,7 +80,7 @@ rm(working_county_acs)
 
 #### import trees per tract, NLCD data, and life expectancy data
 raw_outcome_data <- 
-  fread("city_trees_per_tract.txt", 
+  fread("outcome_data_2018_12_12.txt", 
         colClasses = 
           list(character = 'GEOID'))
 
@@ -93,7 +94,7 @@ data_for_analysis <-
   merge(x = working_sac_city_acs, 
         y = working_outcome_data, 
         by = "GEOID", 
-        all = TRUE)
+        all.x = TRUE)
 
 
 
@@ -107,7 +108,7 @@ data_for_analysis$total_population_B01001e1 <-
 data_for_analysis$population_density_B01001_ALAND <- 
   (1000000*
      data_for_analysis$total_population_B01001e1/
-     data_for_analysis$ALAND)
+     data_for_analysis$ALAND.x)
 
 # total for race purposes
 data_for_analysis$total_for_race_B02001e1 <- 
@@ -121,8 +122,7 @@ data_for_analysis$white_number_B02008e1 <-
 data_for_analysis$white_margin_B02008m1 <- 
   data_for_analysis$B02008e1
 data_for_analysis$white_percent <- 
-  data_for_analysis$number_white/
-  data_for_analysis$total_for_race_B02001e1
+  data_for_analysis$white_number_B02008e1/data_for_analysis$total_for_race_B02001e1
 
 # nhopi
 data_for_analysis$nhopi_number_B02012e1 <-
@@ -368,6 +368,406 @@ data_for_analysis$age_under_5_percent_B01001 <-
 
 
 
+
+#### quick analysis table ####
+
+
+#### variable naming ####
+data_quick_analysis <- data_for_analysis[,c("GEOID","B01001e1")]
+View(data_quick_analysis)
+
+# total population
+data_quick_analysis$total_population_B01001e1 <- 
+  data_for_analysis$B01001e1
+
+# population density
+data_quick_analysis$population_density_B01001_ALAND <- 
+  (1000000*
+     data_for_analysis$total_population_B01001e1/
+     data_for_analysis$ALAND.x)
+
+# total for race purposes
+data_quick_analysis$total_for_race_B02001e1 <- 
+  data_for_analysis$B02001e1
+data_quick_analysis$total_for_race_margin_B02001m1 <- 
+  data_for_analysis$B02001m1
+
+# white
+data_quick_analysis$white_number_B02008e1 <- 
+  data_for_analysis$B02008e1
+data_quick_analysis$white_margin_B02008m1 <- 
+  data_for_analysis$B02008e1
+data_quick_analysis$white_percent <- 
+  data_for_analysis$white_number_B02008e1/
+  data_for_analysis$total_for_race_B02001e1
+
+# nhopi
+data_quick_analysis$nhopi_number_B02012e1 <-
+  data_for_analysis$B02012e1 
+data_quick_analysis$nhopi_margin_B02012m1 <- 
+  data_for_analysis$B02012m1
+data_quick_analysis$nhopi_percent <- 
+  data_for_analysis$nhopi_number_B02012e1/
+  data_for_analysis$total_for_race_B02001e1
+
+# native american alaska native
+data_quick_analysis$aian_number_B02010e1 <-
+  data_for_analysis$B02010e1 
+data_quick_analysis$aian_margin_B02010m1 <- 
+  data_for_analysis$B02010m1
+data_quick_analysis$aian_percent <- 
+  data_for_analysis$aian_number_B02010e1/
+  data_for_analysis$total_for_race_B02001e1
+
+# asian
+data_quick_analysis$asian_number_B02011e1 <-
+  data_for_analysis$B02011e1 
+data_quick_analysis$asian_margin_B02011m1 <- 
+  data_for_analysis$B02011m1
+data_quick_analysis$asian_percent <- 
+  data_for_analysis$asian_number_B02011e1/
+  data_for_analysis$total_for_race_B02001e1
+
+# african american
+data_quick_analysis$black_number_B02009e1 <-
+  data_for_analysis$B02009e1 
+data_quick_analysis$black_margin_B02009m1 <- 
+  data_for_analysis$B02009m1
+data_quick_analysis$black_percent <- 
+  data_for_analysis$black_number_B02009e1/
+  data_for_analysis$total_for_race_B02001e1
+
+# hispanic/latinx
+data_quick_analysis$total_for_ethnicity_B03003e1 <- 
+  data_for_analysis$B03003e1
+data_quick_analysis$margin_for_ethnicity_B03003m1 <- 
+  data_for_analysis$B03003m1
+
+data_quick_analysis$hisp_lat_number_B03003e3 <-
+  data_for_analysis$B03003e3 
+data_quick_analysis$hisp_lat_margin_B03003m3 <-
+  data_for_analysis$B03003m3
+
+data_quick_analysis$not_hisp_lat_number_B03003e2 <-
+  data_for_analysis$B03003e2 
+data_quick_analysis$not_hisp_lat_margin_B03003m2 <-
+  data_for_analysis$B03003m2
+
+data_quick_analysis$hisp_lat_percent_B03003 <- 
+  data_for_analysis$hisp_lat_number_B03003e3/
+  data_for_analysis$total_for_ethnicity_B03003e1
+
+# % below poverty
+data_quick_analysis$poverty_total_B17001e1 <- 
+  data_for_analysis$B17001e1
+data_quick_analysis$poverty_total_margin_B17001m1 <- 
+  data_for_analysis$B17001m1
+
+data_quick_analysis$poverty_below_number_B17001e2 <- 
+  data_for_analysis$B17001e2
+data_quick_analysis$poverty_below_margin_B17001m2 <- 
+  data_for_analysis$B17001m2
+
+data_quick_analysis$poverty_above_number_B17001e31 <- 
+  data_for_analysis$B17001e31
+data_quick_analysis$poverty_above_margin_B17001m31 <- 
+  data_for_analysis$B17001m31
+
+data_quick_analysis$poverty_below_percent_B17001 <- 
+  data_for_analysis$poverty_below_number_B17001e2/
+  data_for_analysis$poverty_total_B17001e1
+
+# unemployment rate
+data_quick_analysis$employment_16_plus_total_S2301e1 <- 
+  data_for_analysis$S2301e1
+data_quick_analysis$employment_16_plus_margin_S2301m1 <- 
+  data_for_analysis$S2301m1
+data_quick_analysis$unemployment_rate_S2301e4 <- 
+  data_for_analysis$S2301e4
+data_quick_analysis$unemployment_rate_margin_S2301m4 <- 
+  data_for_analysis$S2301m4
+
+# educational attainment
+data_quick_analysis$education_total_B15003e1 <- 
+  data_for_analysis$B15003e1
+data_quick_analysis$education_total_margin_B15003m1 <- 
+  data_for_analysis$B15003m1
+
+data_quick_analysis$completed_high_school_number_B15003e17_25 <- 
+  (data_for_analysis$B15003e17 + 
+     data_for_analysis$B15003e18 + 
+     data_for_analysis$B15003e19 + 
+     data_for_analysis$B15003e20 + 
+     data_for_analysis$B15003e21 + 
+     data_for_analysis$B15003e22 + 
+     data_for_analysis$B15003e23 + 
+     data_for_analysis$B15003e24 + 
+     data_for_analysis$B15003e25)
+
+data_quick_analysis$did_not_complete_high_school_number_B15003e2_16 <- 
+  (data_for_analysis$B15003e2 + 
+     data_for_analysis$B15003e3 + 
+     data_for_analysis$B15003e4 + 
+     data_for_analysis$B15003e5 + 
+     data_for_analysis$B15003e6 + 
+     data_for_analysis$B15003e7 + 
+     data_for_analysis$B15003e8 + 
+     data_for_analysis$B15003e9 + 
+     data_for_analysis$B15003e10 + 
+     data_for_analysis$B15003e11 + 
+     data_for_analysis$B15003e12 + 
+     data_for_analysis$B15003e13 + 
+     data_for_analysis$B15003e14 + 
+     data_for_analysis$B15003e15 + 
+     data_for_analysis$B15003e16) # figure out margins later
+
+data_quick_analysis$completed_high_school_percent_B15003 <- 
+  data_for_analysis$completed_high_school_number_B15003e17_25/
+  data_for_analysis$B15003e1
+
+# median age
+data_quick_analysis$median_age_B01002e2 <- 
+  data_for_analysis$B01002e2
+data_quick_analysis$median_age_margin_B01002m2 <- 
+  data_for_analysis$B01002m2
+
+# linguistic isolation
+data_quick_analysis$ling_iso_total_households_S1602e1 <- 
+  data_for_analysis$S1602e1
+data_quick_analysis$ling_iso_total_households_S1602m1 <- 
+  data_for_analysis$S1602m1
+
+data_quick_analysis$ling_iso_number_S1602e3 <- 
+  data_for_analysis$S1602e3
+data_quick_analysis$ling_iso_number_margin_S1602m3 <- 
+  data_for_analysis$S1602m3
+
+data_quick_analysis$ling_iso_percent_S1602e4 <- 
+  data_for_analysis$S1602e4
+data_quick_analysis$ling_iso_percent_margin_S1602m4 <- 
+  data_for_analysis$S1602m4
+
+# housing tenure
+data_quick_analysis$housing_tenure_total_B25003e1 <- 
+  data_for_analysis$B25003e1
+data_quick_analysis$housing_tenure_total_margin_B25003m1 <- 
+  data_for_analysis$B25003m1
+
+data_quick_analysis$owner_occupy_number_B25003e2 <- 
+  data_for_analysis$B25003e2
+data_quick_analysis$owner_occupy_margin_B25003m2 <- 
+  data_for_analysis$B25003m2
+
+data_quick_analysis$renter_occupy_number_B25003e3 <- 
+  data_for_analysis$B25003e3
+data_quick_analysis$renter_occupy_margin_B25003m3 <- 
+  data_for_analysis$B25003m3
+
+data_quick_analysis$owner_occupy_percent_B25003 <- 
+  data_for_analysis$owner_occupy_number_B25003e2/
+  data_for_analysis$housing_tenure_total_B25003e1
+
+data_quick_analysis$renter_occupy_percent_B25003 <- 
+  data_for_analysis$renter_occupy_number_B25003e3/
+  data_for_analysis$housing_tenure_total_B25003e1
+
+# median household income
+data_quick_analysis$median_household_income_B19013e1 <- 
+  data_for_analysis$B19013e1
+data_quick_analysis$median_household_income_margin_B19013m1 <- 
+  data_for_analysis$B19013m1
+
+# median household value
+data_quick_analysis$median_household_value_B25077e1 <- 
+  data_for_analysis$B25077e1
+data_quick_analysis$median_household_value_margin_B25077m1 <- 
+  data_for_analysis$B25077m1
+
+# age below 5
+data_quick_analysis$age_total_B01001e1 <- 
+  data_for_analysis$B01001e1
+data_quick_analysis$age_total_margin_B010001m1 <- 
+  data_for_analysis$B01001m1
+
+data_quick_analysis$age_below_5_number_B01001e3e27 <- 
+  (data_for_analysis$B01001e3 + 
+     data_for_analysis$B01001e27)
+
+data_quick_analysis$age_over_5_number_B01001 <- 
+  (data_for_analysis$B01001e4 + 
+     data_for_analysis$B01001e5 + 
+     data_for_analysis$B01001e6 + 
+     data_for_analysis$B01001e7 + 
+     data_for_analysis$B01001e8 + 
+     data_for_analysis$B01001e9 + 
+     data_for_analysis$B01001e10 + 
+     data_for_analysis$B01001e11 + 
+     data_for_analysis$B01001e12 + 
+     data_for_analysis$B01001e13 + 
+     data_for_analysis$B01001e14 + 
+     data_for_analysis$B01001e15 + 
+     data_for_analysis$B01001e16 + 
+     data_for_analysis$B01001e17 + 
+     data_for_analysis$B01001e18 + 
+     data_for_analysis$B01001e19 + 
+     data_for_analysis$B01001e20 + 
+     data_for_analysis$B01001e21 + 
+     data_for_analysis$B01001e22 + 
+     data_for_analysis$B01001e23 + 
+     data_for_analysis$B01001e24 + 
+     data_for_analysis$B01001e25 +
+     data_for_analysis$B01001e28 + 
+     data_for_analysis$B01001e29 + 
+     data_for_analysis$B01001e30 + 
+     data_for_analysis$B01001e31 + 
+     data_for_analysis$B01001e32 + 
+     data_for_analysis$B01001e33 + 
+     data_for_analysis$B01001e34 + 
+     data_for_analysis$B01001e35 + 
+     data_for_analysis$B01001e36 + 
+     data_for_analysis$B01001e37 + 
+     data_for_analysis$B01001e38 + 
+     data_for_analysis$B01001e39 + 
+     data_for_analysis$B01001e40 + 
+     data_for_analysis$B01001e41 + 
+     data_for_analysis$B01001e42 + 
+     data_for_analysis$B01001e43 + 
+     data_for_analysis$B01001e44 + 
+     data_for_analysis$B01001e45 + 
+     data_for_analysis$B01001e46 + 
+     data_for_analysis$B01001e47 + 
+     data_for_analysis$B01001e48 + 
+     data_for_analysis$B01001e49)
+
+data_quick_analysis$age_under_5_percent_B01001 <- 
+  data_for_analysis$age_below_5_number_B01001e3e27/
+  data_for_analysis$age_total_B01001e1
+
+
+
+#### add outcomes ####
+data_quick_analysis <- 
+  merge(x = data_quick_analysis, 
+        y = working_outcome_data, 
+        by = "GEOID", 
+        all.x = TRUE)
+
+data_quick_analysis$mean_tree_canopy <- data_quick_analysis$MEAN
+
+data_quick_analysis$city_tree_count <- data_quick_analysis$Count_
+
+data_quick_analysis$median_household_value_B25077e1 <- as.numeric(data_quick_analysis$median_household_value_B25077e1)
+
+#### correlation analyses ####
+
+names(data_quick_analysis)
+# make dataframe for the correlation dataset
+correlation_matrix_data  <-  data_quick_analysis[,c("population_density_B01001_ALAND","median_household_income_B19013e1","median_household_value_B25077e1","white_percent","nhopi_percent","aian_percent","asian_percent","black_percent","hisp_lat_percent_B03003","completed_high_school_percent_B15003","median_age_B01002e2", "poverty_below_percent_B17001","unemployment_rate_S2301e4","ling_iso_percent_S1602e4","renter_occupy_percent_B25003","age_under_5_percent_B01001","mean_tree_canopy","city_tree_count","life_expect")]
+
+View(correlation_matrix_data)
+
+# create the correlation matrix
+cormat  <-  round(cor(correlation_matrix_data[,1:19], use = "pairwise.complete.obs"),2) # rounds to 2 decimal places
+
+# view and write to file
+View(cormat)
+write.csv(cormat, file = "cormat.csv")
+
+library(reshape2)
+melted_cormat <- melt(cormat)
+head(melted_cormat)
+
+ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
+  geom_tile()
+
+# Get lower triangle of the correlation matrix
+get_lower_tri<-function(cormat){
+  cormat[upper.tri(cormat)] <- NA
+  return(cormat)
+}
+# Get upper triangle of the correlation matrix
+get_upper_tri <- function(cormat){
+  cormat[lower.tri(cormat)]<- NA
+  return(cormat)
+}
+
+upper_tri <- get_upper_tri(cormat)
+upper_tri
+
+# Melt the correlation matrix
+library(reshape2)
+melted_cormat <- melt(upper_tri, na.rm = TRUE)
+# Heatmap
+library(ggplot2)
+ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed()
+
+reorder_cormat <- function(cormat){
+  # Use correlation between variables as distance
+  dd <- as.dist((1-cormat)/2)
+  hc <- hclust(dd)
+  cormat <-cormat[hc$order, hc$order]
+}
+
+# Reorder the correlation matrix
+cormat <- reorder_cormat(cormat)
+upper_tri <- get_upper_tri(cormat)
+# Melt the correlation matrix
+melted_cormat <- melt(upper_tri, na.rm = TRUE)
+# Create a ggheatmap
+ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed()
+# Print the heatmap
+print(ggheatmap)
+
+ggheatmap + 
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.6, 0.7),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+
+ggheatmap + 
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 3) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.6, 0.7),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+
+print(ggheatmap)
+
+life_expec <- 
+
 #### national data ####
 
 #### aian ####
@@ -558,7 +958,7 @@ ggplot(latino_data_sac_alone,
             vjust=1, 
             hjust = 0.5, 
             family = "serif") +
-  labs(x = paste(expression("National origin of those classified as Hispanic or Latino alone or in any combination in the American Community Survey 2013–2017 five-year estimates (Table B03001)",italic("n "),"= 489,650 +/- 100)")), y = "Number (margin of error)") +
+  labs(x = expression(paste("National origin of those classified as Hispanic or Latino alone or in any combination in the American Community Survey 2013–2017 five-year estimates (Table B03001) (",italic("n "),"= 138,483 +/- 2.706)")), y = "Number (margin of error)") +
   theme_bw() + 
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
@@ -574,17 +974,6 @@ ggplot(latino_data_sac_alone,
 
 
 
-#### correlation analysis ####
-data_for_analysis$GEOID
 
 
-
-#### graph race/ethnicity ####
-
-
-
-#### graph Hispanic/Latinx subgroups
-
-
-#### graph
 
